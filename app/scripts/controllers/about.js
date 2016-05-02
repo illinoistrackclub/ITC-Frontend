4869d3d8a37636ab8bafb06f75e19e119a2c36a4
mkreiser <mkreise2@illinois.edu>
2016-05-02 16:53:35 -0500

    Scaffold App

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name itcFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the itcFrontendApp
+ */
+angular.module('itcFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
