024eb25e8eca7d157435f767639bfdecd4bec6b1
mkreiser <mkreise2@illinois.edu>
2016-06-13 20:21:07 -0500

    Move URLs to separate file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,46 +9,4 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/news', {
-        templateUrl: 'views/news.html',
-        controller: 'NewsCtrl'
-      })
-      .when('/races', {
-        templateUrl: 'views/races.html',
-        controller: 'RacesCtrl'
-      })
-      .when('/clubrelays', {
-        templateUrl: 'views/clubrelays.html',
-        controller: 'ClubRelaysCtrl'
-      })
-      .when('/results', {
-        templateUrl: 'views/results.html',
-        controller: 'ResultsCtrl'
-      })
-      .when('/prospective_members', {
-        templateUrl: 'views/prospective_members.html',
-        controller: 'ProspectiveMembersCtrl'
-      })
-      .when('/current_members/board_minutes', {
-        templateUrl: 'views/current_members/board_minutes.html',
-        controller: 'BoardMinutesCtrl'
-      })
-      .when('/current_members/training_resources', {
-        templateUrl: 'views/current_members/training_resources.html',
-        controller: 'TrainingResourcesCtrl'
-      })
-      .when('contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  ]);
