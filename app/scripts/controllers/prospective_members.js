08c4bf3a1d652320685be41ff6acdad7e97bcfed
mkreiser <mkreise2@illinois.edu>
2016-06-13 21:32:52 -0500

    Add email survey dialog to prospective members

--- app/scripts/controllers/prospective_members.js
+++ app/scripts/controllers/prospective_members.js
@@ -2,6 +2,28 @@
 
 angular.module('itcFrontendApp')
 
-.controller('ProspectiveMembersCtrl', function() {
+.controller('ProspectiveMembersCtrl', function($mdDialog, $scope) {
+  $scope.openEmailSurvey = function() {
+    $mdDialog.show({
+      templateUrl: 'views/dialogs/emailSurvey.html',
+      clickOutsideToClose: false,
+      fullscreen: true,
+      scope: $scope
+    });
+  };
 
+  $scope.submitSurvey = function() {
+    $mdDialog.show(
+      $mdDialog.alert({
+        clickOutsideToClose: true,
+        title: 'Thank You!',
+        textContent: 'You will be added to our email list shortly',
+        ok: 'Continue'
+      })
+    );
+  };
+
+  $scope.cancelSurvey = function() {
+    $mdDialog.cancel();
+  };
 });
