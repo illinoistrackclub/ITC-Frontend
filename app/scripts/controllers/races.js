6352bf26dd337547102af98cdfe81d1f871f940a
mkreiser <mkreise2@illinois.edu>
2016-06-28 21:34:53 -0500

    Add Race schedule page

--- app/scripts/controllers/races.js
+++ app/scripts/controllers/races.js
@@ -2,6 +2,30 @@
 
 angular.module('itcFrontendApp')
 
-.controller('RacesCtrl', function() {
-
+.controller('RacesCtrl', function($scope) {
+  $scope.indoorMeets = [{
+    event: 'Sample Meet',
+    date: 'January 20, 2017',
+    location: 'Champaign, IL'
+  }, {
+    event: 'Sample Meet',
+    date: 'January 20, 2017',
+    location: 'Champaign, IL'
+  }, {
+    event: 'Sample Meet',
+    date: 'January 20, 2017',
+    location: 'Champaign, IL'
+  }, {
+    event: 'Sample Meet',
+    date: 'January 20, 2017',
+    location: 'Champaign, IL'
+  }, {
+    event: 'Sample Meet',
+    date: 'January 20, 2017',
+    location: 'Champaign, IL'
+  }, {
+    event: 'Sample Meet',
+    date: 'January 20, 2017',
+    location: 'Champaign, IL'
+  }];
 });
--- test/spec/controllers/races.js
+++ test/spec/controllers/races.js
@@ -5,11 +5,16 @@ describe('Controller: RacesCtrl', function () {
   // load the controller's module
   beforeEach(module('itcFrontendApp'));
 
+  var $scope;
   var RacesCtrl;
 
   // Initialize the controller
-  beforeEach(inject(function ($controller) {
-    RacesCtrl = $controller('RacesCtrl', {});
+  beforeEach(inject(function ($controller, _$rootScope_) {
+    $scope = _$rootScope_.$new();
+
+    RacesCtrl = $controller('RacesCtrl', {
+      $scope: $scope
+    });
   }));
 
   it('contains spec with an expectation', function() {
