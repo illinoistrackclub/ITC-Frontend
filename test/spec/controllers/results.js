bb382e09b2aedfb0197ae3c8a463a4305bf31952
Mike Kreiser <mkreise2@illinois.edu>
2016-07-06 22:30:26 -0500

    Update Results test

--- test/spec/controllers/results.js
+++ test/spec/controllers/results.js
@@ -5,11 +5,16 @@ describe('Controller: ResultsCtrl', function () {
   // load the controller's module
   beforeEach(module('itcFrontendApp'));
 
+  var $scope;
   var ResultsCtrl;
 
   // Initialize the controller
-  beforeEach(inject(function ($controller) {
-    ResultsCtrl = $controller('ResultsCtrl', {});
+  beforeEach(inject(function ($controller, _$rootScope_) {
+    $scope = _$rootScope_.$new();
+
+    ResultsCtrl = $controller('ResultsCtrl', {
+      $scope: $scope
+    });
   }));
 
   it('contains spec with an expectation', function() {
