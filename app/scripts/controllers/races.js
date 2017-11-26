2d9b3ab10a0a863497dcd58374bcb943bc1e2030
Muneeb Ansari <muneeb@Muneebs-MacBook-Air.local>
2017-11-26 15:36:38 -0600

    fixing meet schedule

--- app/scripts/controllers/races.js
+++ app/scripts/controllers/races.js
@@ -11,50 +11,46 @@ angular.module('itcFrontendApp')
   };
 
   $scope.crossMeets = [{
-    event: 'EIU Walt Crawford Invite',
-    date: 'August 28, 2016',
-    location: 'Charleston, IL'
+    event: 'Aurora Spartan Classic',
+    date: 'September 8, 2017',
+    location: 'Aurora, IL'
   }, {
     event: 'Augustana Brissman-Lundeen Invitational',
-    date: 'September 12, 2016',
+    date: 'September 22, 2017',
     location: 'Rock Island, IL'
   }, {
     event: 'Lucian Rosa Invitational',
-    date: 'October 9, 2016',
+    date: 'October 7, 2017',
     location: 'Somers, WI'
   }, {
     event: 'NIRCA Cross Country Regionals',
-    date: 'October 23, 2016',
-    location: 'Ames, IA'
+    date: 'October 21, 2017',
+    location: 'Iowa City, IA'
   }, {
     event: 'NIRCA Cross Country Nationals',
-    date: 'November 11, 2016',
-    location: 'Hershey, PA'
+    date: 'November 11, 2017',
+    location: 'East Lansing, MI'
   }];
 
   $scope.indoorMeets = [{
+    event: 'Titan First Chance',
+    date: 'December 2, 2017',
+    location: 'Bloomington, IL'
+  }, {
+    event: 'Illini Open***',
+    date: 'January 13, 2018',
+    location: 'Champaign, IL'
+  },{
     event: 'John Craft Invite',
-    date: 'January 21, 2017',
+    date: 'January 20, 2018',
     location: 'Charleston, IL'
   }, {
     event: 'IWU Titan Open',
-    date: 'January 28, 2017',
+    date: 'January 27, 2018',
     location: 'Bloomington, IL'
   }, {
-    event: 'ONU Invite',
-    date: 'February 4, 2017',
-    location: 'Bourbonnais, IL'
-  }, {
-    event: 'GVSU Big Meet',
-    date: 'February 11, 2017',
-    location: 'Allendale, MI'
-  }, {
-    event: 'Monmouth Invitational',
-    date: 'February 11, 2017',
-    location: 'Monmouth, IL'
-  }, {
     event: 'Illinois Club Relays',
-    date: 'February 25, 2017',
+    date: 'February 24, 2018',
     location: 'Champaign, IL',
     important: true,
     link: 'home_meets/clubrelays'
@@ -62,39 +58,31 @@ angular.module('itcFrontendApp')
 
   $scope.outdoorMeets = [{
     event: 'Rose Hulman',
-    date: 'March 18, 2017',
+    date: 'March 17, 2018',
     location: 'Terre Haute, IN'
   }, {
     event: 'WashU Invite',
-    date: 'March 31, 2017',
+    date: 'March 30, 2018',
     location: 'St. Louis, MO'
   }, {
-    event: 'Lindenwood John Creer Invitational',
-    date: 'April 1, 2017',
-    location: 'St. Charles, MO'
-  }, {
     event: 'NIRCA Track & Field Nationals',
-    date: 'April 8-9, 2017',
+    date: 'April 14-15, 2017',
     location: 'Bloomington, IN',
     important: true,
     //link: 'http://www.clubrunning.org/races/race_info.php?race=460'
   }, {
-    event: 'Illinois Twilight',
-    date: 'April 22, 2017',
-    location: 'Champaign, IL'
-  }, {
     event: 'Illinois Club Peaks',
-    date: 'April 29, 2017',
+    date: 'April 28, 2018',
     location: 'Champaign, IL',
     important: true,
     link: 'home_meets/clubpeaks'
   }, {
     event: 'NCC Dr. Keeler',
-    date: 'May 11-12, 2017',
+    date: 'May 10-11, 2018',
     location: 'Naperville, IL'
   }, {
     event: 'NCC Gregory',
-    date: 'May 17-18, 2017',
+    date: 'May 16-17, 2018',
     location: 'Naperville, IL'
   }];
 });
