e2f17deeeb473f4490a5719a7a4d71bd259824a0
Muneeb Ansari <muneeb@Muneebs-MacBook-Air.local>
2018-01-13 20:09:56 -0600

    Adding FAQs to Prospective members

--- app/env/local/apiUrl.js
+++ app/env/local/apiUrl.js
@@ -4,6 +4,6 @@ angular.module('itcFrontendApp')
 
 .service('ApiUrl', function() {
   this.getUrl = function() {
-    return 'http://localhost:8000/';
+    return 'http://illinoistrackclub.herokuapp.com/';
   };
 });
