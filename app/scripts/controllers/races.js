0669c3fb0ce5ea30990ffd353b3625c710990e22
illinoistrackclub <illinoistfclub@gmail.com>
2020-03-03 19:14:23 -0600

    track outdoor schedule

--- app/scripts/controllers/races.js
+++ app/scripts/controllers/races.js
@@ -11,91 +11,68 @@ angular.module('itcFrontendApp')
   };
 
   $scope.crossMeets = [{
-    event: 'Spartan Classic',
-    date: 'September 13, 2019',
-    location: 'Aurora, IL'
-  }, {
-    event: 'Brissman/Lundeen Invitational',
-    date: 'October 4, 2019',
-    location: 'Rock Island, IL'
-  }, {
-    event: 'Lewis Crossover',
-    date: 'October 12, 2019',
-    location: 'Romeoville, IL'
-  }, {
-    event: 'NIRCA Cross Country Regionals',
-    date: 'November 2, 2019',
-    location: 'Urbana, IL'
-  }, {
+    event: 'TBA',
+    date: 'TBA',
+    location: 'TBA'
+  },{
+    event: 'NIRCA Great Plains Regional',
+    date: 'TBA',
+    location: 'TBA'
+  },{
     event: 'NIRCA Cross Country Nationals',
-    date: 'November 16, 2019',
-    location: 'Hanover County, VA'
+    date: 'TBA',
+    location: 'Mechanicsville, VA'
   }];
 
   $scope.indoorMeets = [{
     event: 'Titan First Chance',
-    date: 'December 1, 2018',
+    date: 'December 7, 2019',
     location: 'Bloomington, IL'
   },{
-    event: 'John Craft Invite',
-    date: 'January 19, 2019',
-    location: 'Charleston, IL'  
-  },{
-    event: 'Titan Open',
-    date: 'January 26, 2019',
+    event: 'IWU Titan Open',
+    date: 'February 1, 2020',
     location: 'Bloomington, IL'  
-  },{
-    event: 'Jim Green Invite',
-    date: 'February 2, 2019',
-    location: 'Jacksonville, IL'
-  }, 
-  {
-    event: 'UIndy Invite',
-    date: 'February 9, 2019',
-    location: 'Indianapolis, IN'
-  }, 
-  {
-    event: 'GVSU Big Meet',
-    date: 'February 9, 2019',
-    location: 'Allendale, MI'
-  }, 
-  {
+  }, {
+    event: 'UIndy Team Invitational',
+    date: 'February 15, 2020',
+    location: 'Indianapolis, IN'  
+  }, {
+    event: 'UChicago Margaret Bradley Invite',
+    date: 'February 22, 2020',
+    location: 'Chicago, IL'  
+  }, {
     event: 'Illinois Club Relays',
-    date: 'February 23, 2019',
+    date: 'February 29, 2020',
     location: 'Champaign, IL',
     important: true,
     link: 'home_meets/clubrelays'
   }];
 
   $scope.outdoorMeets = [{
-    event: 'WashU Invitational',
-    date: 'March 29, 2019',
-    location: 'St. Louis, MO'
-  },{
-    event: 'John Creer Invitational',
-    date: 'March 30, 2019',
-    location: 'Lindenwood, MO'
-  },{
-    event: 'Millikin',
-    date: 'April 6, 2019',
-    location: 'Decatur, IL'
+    event: 'IWU Polar Bear Invitational',
+    date: 'March 28, 2020',
+    location: 'Marion, IN'
   },{
     event: 'NIRCA Track & Field Nationals',
-    date: 'April 13-14, 2019',
+    date: 'April 4-5, 2020',
     location: 'Oxford, OH',
     important: true,
     // link: 'https://clubrunning.org/races/race_info.php?race=766'
   },{
+    event: 'EIU Big Blue Classic',
+    date: 'April 11, 2020',
+    location: 'Charleston, IL'
+  },{
     event: 'True Blue Open',
-    date: 'April 27, 2019',
+    date: 'April 25, 2020',
     location: 'Jacksonville, IL'
   },{
-    event: 'NCC Dr. Keeler',
-    date: 'May 9-10, 2019',
+    event: 'Dr. Keeler Invitational',
+    date: 'May 7-8, 2020',
     location: 'Naperville, IL'
   },{
-    event: 'NCC Gregory',
-    date: 'May 15-16, 2019',
+    event: 'Gregory Invitational',
+    date: 'May 13-14, 2020',
     location: 'Naperville, IL'
   }];
 });
